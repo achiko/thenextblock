c16f2c01c102f4f49ffa7c05e2d475ce5e6ce3b4
Archil Phanchulidze <achiko@gmail.com>
2017-12-25 21:33:06 +0400

    last update

--- nextblock.sol
+++ nextblock.sol
@@ -5,8 +5,8 @@ contract TheNextBlock {
     bool isBetEnabled = true; 
 
     //How many guesses you will need in a row to win and get money.
-    uint8 public requiredGuessCount = 5;
-    //How many percent owners take from every win.
+    uint8 public requiredGuessCount = 2;
+    //How many percent can take owners from every win.
     uint8 public ownerProfitPercent = 10;
     //Next Jackpot starting balance percent
     uint8 public nextJackpotBalancePercent = 20;
@@ -19,14 +19,27 @@ contract TheNextBlock {
     uint256 public allowedBetAmount = 10000000000000000; // 0.01 ETH
     //Map of guesses, here is stored who how many time guessed.
     //After every wrong guess counter goes to 0.
-    mapping(address => uint8) playersGuessCounts;
-        
-    struct Winner {
-        address addr;
+    uint256 public blockedBalance = 0;
+    
+    // Players struct
+    struct PlayerData {
+        uint256 balance;
+        uint256[] winningBlocks;
+    }
+    // Players data
+    mapping(address => PlayerData) public PlayersData;
+    
+    mapping(address => uint8) public playersGuessCounts;
+    //Wining Table stores everything to execute win and give reward everywon who won.
+    struct WiningTable {
+        bool executed;
+        uint256 balance;
+        address[] jackPotWinners;
+        address[] blockBetters;
     }
     
     //Winners Mapping
-    mapping (uint256 => Winner[] ) winners;
+    mapping (uint256 => WiningTable) public wTables;
         
     event BetReceived(uint256 sender, uint256 value, uint256 balance);
     event BetIsLess(uint256 sender, uint256 value, uint256 balance);
@@ -37,6 +50,7 @@ contract TheNextBlock {
     event Log(string value);
     event JackPot(address winner);
     event LogMiner(address miner);
+    event LogUint(uint256 value);
 
     //Contract owner address
     address public owner;
@@ -82,23 +96,55 @@ contract TheNextBlock {
         notMore {
             
             LogMiner(block.coinbase);
-            
+            // if player guessed block miner 
             if(block.coinbase == _miner) {
-                if(playersGuessCounts[msg.sender] != address(0x0) ) {
+                // if player exists in list
+                if(playersGuessCounts[msg.sender] != address(0x0)) {
                     playersGuessCounts[msg.sender] += 1;
+                    // if player reached winning count 
                     if(playersGuessCounts[msg.sender] == requiredGuessCount) {
-                        winners[block.number].push(Winner(msg.sender));
-                    }
+                        LogUint(wTables[block.number].balance);
+                        if(wTables[block.number].balance == 0 ) {
+                            Log("First Jackpot Winner !!! ");
+                            wTables[block.number].executed = false;
+                            wTables[block.number].balance = this.balance - blockedBalance;
+                            wTables[block.number].jackPotWinners.push(msg.sender);
+                            blockedBalance += this.balance - blockedBalance;
+                        } else {
+                            Log("First Jackpot Winner !!! ");
+                            wTables[block.number].jackPotWinners.push(msg.sender);
+                        }
+                        
+                        // reset winners count 
+                        playersGuessCounts[msg.sender] = 0;
+                    } 
+                    //Here contracts needs to make call to itself which will be executed in new block.
+                    //Call is made to a function which will execute wining table and send funds.
+                }else{ 
+                    playersGuessCounts[msg.sender] = 1;
                 }  
             }else{
-                if(playersGuessCounts[msg.sender] != address(0x0) ) {
+                if(playersGuessCounts[msg.sender] != address(0x0)) {
                     playersGuessCounts[msg.sender] = 0;
                 }
             }
+    }
+    
+    function executeWinnigTable(uint256  blockNumber) public {
+        if(wTables[blockNumber].executed == false) {
+            WiningTable memory tbl = wTables[blockNumber];
+            // for(uint256 i=0; i < tbl.jackPotWinners.lenght; i++ ) {
+                
+            // } 
+        }
+    }
+    
+    function getJackpotWinnersByBlockNumber(uint256 blockNumber) public view returns(address[]) {
+        return wTables[blockNumber].jackPotWinners;
     }
 
     function getBalance() public view returns(uint256) {
         return this.balance;
     }
 
-}   
+} 
