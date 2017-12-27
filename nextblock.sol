45600568f961b61bcf255156a8f5debca93e187e
svireli <g.lominashvili@gmail.com>
2017-12-27 06:45:16 +0000

    refresh contract.

--- nextblock.sol
+++ nextblock.sol
@@ -2,8 +2,17 @@ pragma solidity ^0.4.19;
 
 contract TheNextBlock {
 
+    event BetReceived(address sender, uint256 value, uint256 balance);
+    event GivingBackTheRest(address sender, uint256 value, uint256 rest);
+    event BetExecuted(address sender, uint256 bet, uint256 value);
+    event Balance(uint256 value);
+    event LogStr(string value);
+    event JackPot(address winner);
+    event LogMiner(address miner);
+    event LogUint(uint256 value);
+    
+    //If this is set to false contract will not receive funds
     bool isBetEnabled = true; 
-
     //How many guesses you will need in a row to win and get money.
     uint8 public requiredGuessCount = 2;
     //How many percent can take owners from every win.
@@ -41,16 +50,6 @@ contract TheNextBlock {
     //Winners Mapping
     mapping (uint256 => WiningTable) public wTables;
         
-    event BetReceived(uint256 sender, uint256 value, uint256 balance);
-    event BetIsLess(uint256 sender, uint256 value, uint256 balance);
-    event BetIsMore(uint256 sender, uint256 value, uint256 balance);
-    event GivingBackTheRest(address sender, uint256 value, uint256 rest);
-    event BetExecuted(address sender, uint256 bet, uint256 value);
-    event Balance(uint256 value);
-    event Log(string value);
-    event JackPot(address winner);
-    event LogMiner(address miner);
-    event LogUint(uint256 value);
 
     //Contract owner address
     address public owner;
@@ -73,61 +72,64 @@ contract TheNextBlock {
         _;
     }
 
-    modifier betEnabled() { 
+    modifier onlyWhenBetIsEnabled() { 
         require(isBetEnabled);
         _; 
     }
     
+    modifier onlyWhenGuessed(address miner) {
+        if(block.coinbase == miner){
+            _;
+        }else{
+            if(playerExistsInGuesCountMap(msg.sender)) {
+                playersGuessCounts[msg.sender] = 0;
+            }
+        }
+    }
     
     function TheNextBlock() public {
-        Log("Congratulations ");
+        owner = msg.sender;
+        LogStr("Congratulations Contract Created!");
     }
 
-
-    function () public payable {
-            
+    //This is left for donations
+    function () public payable { }
+    
+    function playerExistsInGuesCountMap(address player) public view returns(bool) {
+        return playersGuessCounts[player] != address(0x0);
     }
     
     function placeBet(address _miner) 
         public 
         payable 
-        betEnabled 
+        onlyWhenBetIsEnabled 
         notLess 
-        notMore {
-            
+        notMore
+        onlyWhenGuessed(_miner) {
+            BetReceived(msg.sender, msg.value, this.balance);
             LogMiner(block.coinbase);
-            // if player guessed block miner 
-            if(block.coinbase == _miner) {
-                // if player exists in list
-                if(playersGuessCounts[msg.sender] != address(0x0)) {
-                    playersGuessCounts[msg.sender] += 1;
-                    // if player reached winning count 
-                    if(playersGuessCounts[msg.sender] == requiredGuessCount) {
-                        LogUint(wTables[block.number].balance);
-                        if(wTables[block.number].balance == 0 ) {
-                            Log("First Jackpot Winner !!! ");
-                            wTables[block.number].executed = false;
-                            wTables[block.number].balance = this.balance - blockedBalance;
-                            wTables[block.number].jackPotWinners.push(msg.sender);
-                            blockedBalance += this.balance - blockedBalance;
-                        } else {
-                            Log("First Jackpot Winner !!! ");
-                            wTables[block.number].jackPotWinners.push(msg.sender);
-                        }
-                        
-                        // reset winners count 
-                        playersGuessCounts[msg.sender] = 0;
-                    } 
-                    //Here contracts needs to make call to itself which will be executed in new block.
-                    //Call is made to a function which will execute wining table and send funds.
-                }else{ 
-                    playersGuessCounts[msg.sender] = 1;
-                }  
-            }else{
-                if(playersGuessCounts[msg.sender] != address(0x0)) {
-                    playersGuessCounts[msg.sender] = 0;
+            playersGuessCounts[msg.sender] += 1;
+            // if player reached winning count 
+            if(playersGuessCounts[msg.sender] == requiredGuessCount) {
+                LogUint(wTables[block.number].balance);
+                if(wTables[block.number].balance == 0 ) {
+                    LogStr("First Jackpot Winner !!! ");
+                    wTables[block.number].executed = false;
+                    wTables[block.number].balance = this.balance - blockedBalance;
+                    wTables[block.number].jackPotWinners.push(msg.sender);
+                    blockedBalance += this.balance - blockedBalance;
+                } else {
+                    LogStr("First Jackpot Winner !!! ");
+                    wTables[block.number].jackPotWinners.push(msg.sender);
                 }
-            }
+                
+                // reset winners count 
+                playersGuessCounts[msg.sender] = 0;
+            } 
+            //Here contracts needs to make call to itself which will be executed in new block.
+            //Call is made to a function which will execute wining table and send funds.
+             
+
     }
     
     function executeWinnigTable(uint256  blockNumber) public {
